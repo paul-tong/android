514369ead2affdaa81d2bb34244fb097201940d1
tong <paul.tong.cs@gmail.com>
2019-09-13 00:05:04 -0400

    dinner decider

---
+++ DinnerDecider/app/src/main/java/com/tong/dinnerdecider/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.tong.dinnerdecider
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    val foodList = mutableListOf("Chinese", "Frence", "Piazza")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val randomIndex = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomIndex]
+        }
+
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodTxt.text.toString()
+            foodList.add(newFood)
+            addFoodTxt.text.clear()
+        }
+    }
+}
