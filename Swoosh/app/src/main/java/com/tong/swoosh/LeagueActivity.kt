c871d84b167ab5bd4b726e639f5b47bca00db146
tong <paul.tong.cs@gmail.com>
2019-09-13 22:18:09 -0400

    add intent

---
+++ Swoosh/app/src/main/java/com/tong/swoosh/LeagueActivity.kt
@@ -0,0 +1,14 @@
+package com.tong.swoosh
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
+
+class LeagueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
